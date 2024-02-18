50f3dba3ce5b7cf5ebed321de2bf30237302dfd9
qwernys <jonsson.kevin01@gmail.com>
2024-02-18 16:55:20 +0100

    Removed roundedimage

--- src/Components/Home/Profile.jsx
+++ src/Components/Home/Profile.jsx
@@ -3,12 +3,6 @@ import MyPhoto from "/src/images/me.jpg"
 
 export default function Profile() {
     return (
-      <ReactRoundedImage
-        image={MyPhoto}
-        roundedColor="#321124"
-        roundedSize="3"
-        borderRadius="100"
-        resizeMode="contain"
-    />
+      <></>
     )
 }
